7597a3e2e1b914067e99d1ea63a8df6f0edf38a6
Snuslik <144604004+Snuslik@users.noreply.github.com>
2024-03-25 14:57:30 +0300

    7 задание

---
+++ 7_Horikov/CAR/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+
+namespace CAR
+{
+    public class Car
+    {
+        private int speed = 0;
+        public bool power = false;
+        private int maxSpeed = 120;
+        private int peredacha = 0;
+        
+        public void changePower()
+        {
+            if (power == false)
+            {
+                power = true;
+            } else 
+            {
+                power = false;
+            }
+        }
+        private void changeSpeed(string setspeed) 
+        {
+            if (setspeed == "true")
+            {
+                speed += 5;
+            } else if (setspeed == "false" & speed != 0)
+            {
+                speed -= 5;
+            }
+        }
+        public int setSpeed(string setspeed)
+        {
+            if (power == true)
+            {
+                if (speed >= 25 & peredacha == 2 & speed <= 35)
+                {
+                    changeSpeed(setspeed);
+                }
+                else if (speed <= 20 & peredacha == 1)
+                {
+                    changeSpeed(setspeed);
+                }
+                else if (speed >= 40 & peredacha == 3 & speed <= 60)
+                {
+                    changeSpeed(setspeed);
+                }
+                else if (speed >= 65 & peredacha == 4 & speed <= 80)
+                {
+                    changeSpeed(setspeed);
+                } else if (setspeed == "false")
+                {
+                    changeSpeed(setspeed);
+                }
+            }
+            return speed;
+        }
+        public int changePeredacha(bool change)
+        {
+            if (change == true)
+            {
+                peredacha += 1;
+            } else if (peredacha>0)
+            {
+                peredacha -= 1;
+            }
+            return peredacha;
+        }
+    }
+}
---
+++ 7_Horikov/poezdka/Program.cs
@@ -0,0 +1,63 @@
+﻿using CAR;
+
+Car car = new();
+Console.WriteLine("Вы сели в машину");
+
+while (true)
+{
+    Console.WriteLine("Чтобы завести машину нужно написать \"1\"");
+    while (car.power == false)
+    {
+        if (Console.ReadLine() == "1")
+        {
+            car.power = true;
+        }
+        if (car.power == true)
+        {
+            Console.WriteLine("Вы завели машину");
+        } else
+        {
+            Console.WriteLine("Вы занимаетесь чем угодно,но машину вы так не завели");
+        }
+    }
+    Console.WriteLine("Чтоб начать набирать скорость нужно написать \"+\"");
+    Console.WriteLine("Чтоб начать збрасывать скорость нужно написать \"-\"");
+    Console.WriteLine("Чтобы переключить передачу нужно паписать \"++\" или \"--\"");
+    Console.WriteLine("Чтобы заглушить машину напишите \"0\"");
+    int speed = 0;
+    while (car.power == true)
+    {
+        string change = Console.ReadLine();
+        if (change == "+")
+        {
+            speed = car.setSpeed("true");
+            Console.WriteLine($"Вы прибавили скорость, теперь на составляет {speed}");
+        } else if (change == "-")
+        {
+            speed = car.setSpeed("false");
+            Console.WriteLine($"Вы немного затормозили, ваша скорость составляет {speed}");
+        } else if (change == "++") 
+        {
+            int peredahcha = car.changePeredacha(true);
+            Console.WriteLine($"Вы переключили передачу, тепеь вы на передаче {peredahcha}");
+        } else if (change == "--")
+        {
+            int peredahcha = car.changePeredacha(false);
+            Console.WriteLine($"Вы переключили передачу, вы на {peredahcha} передаче");
+        } else if (change == "заглушить")
+        {
+            car.power = false;
+            Console.WriteLine("Вы заглушили машину");
+            if (speed > 0)
+            {
+                Console.WriteLine("Так как вы не остановились перед тек как заглушить машину, вы вылетели с дороги и умерли");
+            } else
+            {
+                Console.WriteLine("Вы спокойно вышли из машины и пошли по свои делам");
+            }
+        } else
+        {
+            Console.WriteLine($"Вы продолжаете ехать со скоростью {car.setSpeed("pupu")}");
+        }
+    }
+}
